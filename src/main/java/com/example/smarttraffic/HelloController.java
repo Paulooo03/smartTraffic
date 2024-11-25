7c9df73bf59606760c09b9f298564cd0982d11ff
Paulooo03 <jose_paulo_caleon@dlsu.edu.ph>
2024-11-25 21:44:53 +0800

    put names on map

--- src/main/java/com/example/smarttraffic/HelloController.java
+++ src/main/java/com/example/smarttraffic/HelloController.java
@@ -1,14 +1,93 @@
 package com.example.smarttraffic;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    @FXML private Line di, qp, ib, ed, bo, gd, pl, ke, lg, rq, sr, jb, lj, ml, um, wu, ce, fc, wf, mg, xm, ht, ph, mp, fm, nf, ao, ta, a1t, ya1, zy, sz, va1, qv, ji, hj, vh, zv, nk;
+    @FXML private Circle aa, bb, cc, dd, ee, ff, gg, hh, ii, jj, kk, ll, mm, nn, oo, pp, qq, rr, ss, tt, uu, vv, ww, xx, yy, zz, a1;
+
+    public void aaClicked(MouseEvent mouseEvent) {
+    }
+
+    public void bbClicked(MouseEvent mouseEvent) {
+    }
+
+    public void ccClicked(MouseEvent mouseEvent) {
+    }
+
+    public void ddClicked(MouseEvent mouseEvent) {
+    }
+
+    public void eeClicked(MouseEvent mouseEvent) {
+    }
+
+    public void ffClicked(MouseEvent mouseEvent) {
+    }
+
+    public void ggClicked(MouseEvent mouseEvent) {
+    }
+
+    public void hhClicked(MouseEvent mouseEvent) {
+    }
+
+    public void iiClicked(MouseEvent mouseEvent) {
+    }
+
+    public void jjClicked(MouseEvent mouseEvent) {
+    }
+
+    public void kkClicked(MouseEvent mouseEvent) {
+    }
+
+    public void llClicked(MouseEvent mouseEvent) {
+    }
+
+    public void mmClicked(MouseEvent mouseEvent) {
+    }
+
+    public void nnClicked(MouseEvent mouseEvent) {
+    }
+
+    public void ooClicked(MouseEvent mouseEvent) {
+    }
+
+    public void ppClicked(MouseEvent mouseEvent) {
+    }
+
+    public void qqClicked(MouseEvent mouseEvent) {
+    }
+
+    public void rrClicked(MouseEvent mouseEvent) {
+    }
+
+    public void ssClicked(MouseEvent mouseEvent) {
+    }
+
+    public void ttClicked(MouseEvent mouseEvent) {
+    }
+
+    public void uuClicked(MouseEvent mouseEvent) {
+    }
+
+    public void vvClicked(MouseEvent mouseEvent) {
+    }
+
+    public void wwClicked(MouseEvent mouseEvent) {
+    }
+
+    public void xxClicked(MouseEvent mouseEvent) {
+    }
+
+    public void yyClicked(MouseEvent mouseEvent) {
+    }
+
+    public void zzClicked(MouseEvent mouseEvent) {
+    }
+
+    public void a1Clicked(MouseEvent mouseEvent) {
     }
 }
