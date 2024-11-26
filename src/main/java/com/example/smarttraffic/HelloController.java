f9dbc1ae45f1bac2c254c02dadfbcf64be2c69ed
12309192 <jose_paulo_caleon@dlsu.edu.ph>
2024-11-26 11:32:46 +0800

    add edges in arraylist

--- src/main/java/com/example/smarttraffic/HelloController.java
+++ src/main/java/com/example/smarttraffic/HelloController.java
@@ -4,55 +4,44 @@ import javafx.fxml.FXML;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import java.util.ArrayList;
 
 public class HelloController {
 
-    @FXML private Line di, qp, ib, ed, bo, gd, pl, ke, lg, rq, sr, jb, lj, ml, um, wu, ce, fc, wf, mg, xm, ht, ph, mp, fm, nf, ao, ta, a1t, ya1, zy, sz, va1, qv, ji, hj, vh, zv, nk;
-    @FXML private Circle aa, bb, cc, dd, ee, ff, gg, hh, ii, jj, kk, ll, mm, nn, oo, pp, qq, rr, ss, tt, uu, vv, ww, xx, yy, zz, a1;
+    public Line qp, pl, lj, ht, ph, ta, a1t, ya1, zy, sz, va1, qv, hj, vh, zv, sq;
+    public Circle aa, hh, jj, ll, pp, qq, ss, tt, vv, yy, zz, a1;
+
+    // Initialize ArrayLists in the constructor
+    public HelloController() {
+        Gypsum = new ArrayList<>();
+        Gypsum.add("sq");
+
+        Cinnabar = new ArrayList<>();
+        Barnie = new ArrayList<>();
+        Cherry = new ArrayList<>();
+        Coral = new ArrayList<>();
+        Barite = new ArrayList<>();
+        Jevite = new ArrayList<>();
+    }
+
+    ArrayList<String> Gypsum;
+    ArrayList<String> Cinnabar;
+    ArrayList<String> Barnie;
+    ArrayList<String> Cherry;
+    ArrayList<String> Coral;
+    ArrayList<String> Barite;
+    ArrayList<String> Jevite;
 
     public void aaClicked(MouseEvent mouseEvent) {
-    }
-
-    public void bbClicked(MouseEvent mouseEvent) {
-    }
-
-    public void ccClicked(MouseEvent mouseEvent) {
-    }
-
-    public void ddClicked(MouseEvent mouseEvent) {
-    }
-
-    public void eeClicked(MouseEvent mouseEvent) {
-    }
-
-    public void ffClicked(MouseEvent mouseEvent) {
-    }
-
-    public void ggClicked(MouseEvent mouseEvent) {
     }
 
     public void hhClicked(MouseEvent mouseEvent) {
     }
 
-    public void iiClicked(MouseEvent mouseEvent) {
-    }
-
     public void jjClicked(MouseEvent mouseEvent) {
     }
 
-    public void kkClicked(MouseEvent mouseEvent) {
-    }
-
     public void llClicked(MouseEvent mouseEvent) {
-    }
-
-    public void mmClicked(MouseEvent mouseEvent) {
-    }
-
-    public void nnClicked(MouseEvent mouseEvent) {
-    }
-
-    public void ooClicked(MouseEvent mouseEvent) {
     }
 
     public void ppClicked(MouseEvent mouseEvent) {
@@ -61,25 +50,13 @@ public class HelloController {
     public void qqClicked(MouseEvent mouseEvent) {
     }
 
-    public void rrClicked(MouseEvent mouseEvent) {
-    }
-
     public void ssClicked(MouseEvent mouseEvent) {
     }
 
     public void ttClicked(MouseEvent mouseEvent) {
     }
 
-    public void uuClicked(MouseEvent mouseEvent) {
-    }
-
     public void vvClicked(MouseEvent mouseEvent) {
-    }
-
-    public void wwClicked(MouseEvent mouseEvent) {
-    }
-
-    public void xxClicked(MouseEvent mouseEvent) {
     }
 
     public void yyClicked(MouseEvent mouseEvent) {
