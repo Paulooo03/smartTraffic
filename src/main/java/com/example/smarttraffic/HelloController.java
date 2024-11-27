93f430159cef271eed2b1c5188d925019749f933
Paulooo03 <jose_paulo_caleon@dlsu.edu.ph>
2024-11-27 14:18:16 +0800

    finished

--- src/main/java/com/example/smarttraffic/HelloController.java
+++ src/main/java/com/example/smarttraffic/HelloController.java
@@ -1,176 +1,143 @@
 package com.example.smarttraffic;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.Line;
+import javafx.scene.shape.Polyline;
 
-import java.util.*;
+import java.util.Arrays;
 
 public class HelloController {
 
     @FXML
-    private Line qp, pl, lj, ht, ph, ta, a1t, ya1, zy, sz, va1, qv, hj, vh, zv, sq, aj;
+    private Button getRoute;
     @FXML
-    private Circle aa, hh, jj, ll, pp, qq, ss, tt, vv, yy, zz, a1;
+    private MenuItem sixam, eightam, tenam, threepm, fivepm, sevenpm;
+    @FXML
+    private Circle pointA, pointB;
+    @FXML
+    private Circle LaSalle, Novaliches;
+    @FXML
+    private Polyline quirino, ortigas, taft, rodriguez, quezon, araneta, mesa, domingo;
 
-    private HashMap<String, String[]> relations;
-    private String firstClickedCircle = null;
-    private String secondClickedCircle = null;
+    //creating of road info
+    //let quirino = q, let ortigas = o, let taft = t, let rodriquez = r, let araneta = a, let mesa = m, let domingo = d, let road = {distance, traffic lights}
 
-    public HelloController() {
-        relations = new HashMap<>();
-        relations.put("aa", new String[]{"aj", "ta"});
-        relations.put("hh", new String[]{"hj", "ph", "ht", "vh"});
-        relations.put("jj", new String[]{"aj", "hj", "lj"});
-        relations.put("ll", new String[]{"lj", "pl"});
-        relations.put("pp", new String[]{"ph", "pl", "qp"});
-        relations.put("qq", new String[]{"qp", "sq", "qv"});
-        relations.put("ss", new String[]{"sq", "sz"});
-        relations.put("tt", new String[]{"ht", "a1t"});
-        relations.put("vv", new String[]{"vh", "qv", "zv", "va1"});
-        relations.put("yy", new String[]{"zy", "ya1"});
-        relations.put("zz", new String[]{"sz", "zv", "zy"});
-        relations.put("a1", new String[]{"a1t", "va1", "ya1"});
+    int[] q = {3500, 7, 0};
+    int[] o = {12000, 5, 0};
+    int[] t = {8900, 8, 0};
+    int[] r = {5000, 4, 0};
+    int[] a = {6300, 9, 0};
+    int[] m = {10000, 11, 0};
+    int[] d = {6000, 4, 0};
+    int[] qz = {7000, 6, 0};
+
+    int[][] routes = {q, o, t, r, a, m, d, qz};
+    private Polyline[] polylines;
+
+    @FXML
+    public void initialize() {
+        polylines = new Polyline[]{quirino, ortigas, taft, rodriguez, araneta, mesa, domingo, quezon};
     }
 
-    public void aaClicked(MouseEvent mouseEvent) { handleCircleClick("aa"); }
-    public void hhClicked(MouseEvent mouseEvent) { handleCircleClick("hh"); }
-    public void jjClicked(MouseEvent mouseEvent) { handleCircleClick("jj"); }
-    public void llClicked(MouseEvent mouseEvent) { handleCircleClick("ll"); }
-    public void ppClicked(MouseEvent mouseEvent) { handleCircleClick("pp"); }
-    public void qqClicked(MouseEvent mouseEvent) { handleCircleClick("qq"); }
-    public void ssClicked(MouseEvent mouseEvent) { handleCircleClick("ss"); }
-    public void ttClicked(MouseEvent mouseEvent) { handleCircleClick("tt"); }
-    public void vvClicked(MouseEvent mouseEvent) { handleCircleClick("vv"); }
-    public void yyClicked(MouseEvent mouseEvent) { handleCircleClick("yy"); }
-    public void zzClicked(MouseEvent mouseEvent) { handleCircleClick("zz"); }
-    public void a1Clicked(MouseEvent mouseEvent) { handleCircleClick("a1"); }
+    public void onClicksix(ActionEvent actionEvent) {
+        q[2] = 60;
+        o[2] = 50;
+        t[2] = 80;
+        r[2] = 50;
+        a[2] = 70;
+        m[2] = 45;
+        d[2] = 60;
+        qz[2] = 70;
+    }
 
-    private void handleCircleClick(String circleName) {
-        if (firstClickedCircle == null) {
-            firstClickedCircle = circleName;
-            handleFirstCircleClick(circleName);
-        } else if (firstClickedCircle.equals(circleName)) {
-            resetLines();
-            firstClickedCircle = null;
-            secondClickedCircle = null;
-        } else {
-            secondClickedCircle = circleName;
-            highlightExtendedRelations(firstClickedCircle, secondClickedCircle);
-            handleFirstCircleClick(firstClickedCircle);
+    public void onClickeightam(ActionEvent actionEvent) {
+        q[2] = 70;
+        o[2] = 60;
+        t[2] = 85;
+        r[2] = 65;
+        a[2] = 65;
+        m[2] = 55;
+        d[2] = 70;
+        qz[2] = 75;
+    }
+
+    public void onClicktenam(ActionEvent actionEvent) {
+        q[2] = 30;
+        o[2] = 40;
+        t[2] = 20;
+        r[2] = 30;
+        a[2] = 40;
+        m[2] = 25;
+        d[2] = 30;
+        qz[2] = 20;
+    }
+
+    public void onClickthreepm(ActionEvent actionEvent) {
+        q[2] = 50;
+        o[2] = 40;
+        t[2] = 65;
+        r[2] = 45;
+        a[2] = 50;
+        m[2] = 75;
+        d[2] = 55;
+        qz[2] = 30;
+    }
+
+    public void onClickfivepm(ActionEvent actionEvent) {
+        q[2] = 75;
+        o[2] = 80;
+        t[2] = 75;
+        r[2] = 80;
+        a[2] = 65;
+        m[2] = 55;
+        d[2] = 70;
+        qz[2] = 50;
+    }
+
+    public void onClicksevenpm(ActionEvent actionEvent) {
+        q[2] = 80;
+        o[2] = 45;
+        t[2] = 76;
+        r[2] = 60;
+        a[2] = 60;
+        m[2] = 55;
+        d[2] = 75;
+        qz[2] = 80;
+    }
+
+    //main algorithm starts here
+    public void getRoute(ActionEvent actionEvent) {
+        double distanceMean = (double) (q[0] + o[0] + t[0] + r[0] + a[0] + m[0] + d[0] + qz[0]) / 7;
+        double lightsMean = (double) (q[1] + o[1] + t[1] + r[1] + a[1] + m[1] + d[1] + qz[1]) / 7;
+        double trafficMean = (double) (q[2] + o[2] + t[2] + r[2] + a[2] + m[2] + d[2]+ qz[2]) / 7;
+
+        // Reset all polyline colors first to avoid keeping any previous highlight
+        for (Polyline polyline : polylines) {
+            polyline.setStroke(Color.BLACK);  // Set default color (black) or any other color
         }
-    }
 
-    private void handleFirstCircleClick(String circleName) {
-        resetLines();
-        String[] relatedCircleNames = relations.get(circleName);
-        if (relatedCircleNames != null) {
-            for (String relatedCircleName : relatedCircleNames) {
-                Line lineToHighlight = findLineBetweenCircles(circleName, relatedCircleName);
-                if (lineToHighlight != null) {
-                    lineToHighlight.setStroke(Color.GREEN);
-                    lineToHighlight.setStrokeWidth(3);
-                }
+        for (int i = 0; i < 8; i++) {
+            if (routes[i][0] >= distanceMean && routes[i][1] >= lightsMean && routes[i][2] >= trafficMean) {
+                polylines[i].setStroke(Color.RED);  // Highlight A (or any other color)
+            } else if (routes[i][0] < distanceMean && routes[i][1] >= lightsMean && routes[i][2] >= trafficMean) {
+                polylines[i].setStroke(Color.GREEN);  // Highlight B
+            } else if (routes[i][0] >= distanceMean && routes[i][1] >= lightsMean && routes[i][2] < trafficMean) {
+                polylines[i].setStroke(Color.YELLOW);  // Highlight C
+            } else if (routes[i][0] >= distanceMean && routes[i][1] < lightsMean && routes[i][2] >= trafficMean) {
+                polylines[i].setStroke(Color.BLUE);  // Highlight D
+            } else if (routes[i][0] < distanceMean && routes[i][1] < lightsMean && routes[i][2] >= trafficMean) {
+                polylines[i].setStroke(Color.PURPLE);  // Highlight E
+            } else if (routes[i][0] < distanceMean && routes[i][1] >= lightsMean && routes[i][2] < trafficMean) {
+                polylines[i].setStroke(Color.ORANGE);  // Highlight F
+            } else if (routes[i][0] >= distanceMean && routes[i][1] < lightsMean && routes[i][2] < trafficMean) {
+                polylines[i].setStroke(Color.CYAN);  // Highlight G
+            } else if (routes[i][0] < distanceMean && routes[i][1] < lightsMean && routes[i][2] < trafficMean) {
+                polylines[i].setStroke(Color.BLACK);  // Highlight H
             }
-        }
-    }
-
-    private void highlightExtendedRelations(String startCircle, String endCircle) {
-        resetLines();
-        Set<List<String>> allPaths = findAllPaths(startCircle, endCircle);
-
-        for (List<String> path : allPaths) {
-            for (int i = 0; i < path.size() - 1; i++) {
-                Line pathLine = findLineBetweenCircles(path.get(i), path.get(i + 1));
-                if (pathLine != null) {
-                    pathLine.setStroke(Color.YELLOW);
-                    pathLine.setStrokeWidth(3);
-                }
-            }
-        }
-
-        firstClickedCircle = null;
-        secondClickedCircle = null;
-    }
-
-    private Set<List<String>> findAllPaths(String start, String end) {
-        Set<List<String>> paths = new HashSet<>();
-        Queue<List<String>> queue = new LinkedList<>();
-        queue.add(Arrays.asList(start));
-
-        while (!queue.isEmpty()) {
-            List<String> path = queue.poll();
-            String last = path.get(path.size() - 1);
-
-            if (last.equals(end)) {
-                paths.add(path);
-            }
-
-            String[] neighbors = relations.get(last);
-            if (neighbors != null) {
-                for (String neighbor : neighbors) {
-                    if (!path.contains(neighbor)) {
-                        List<String> newPath = new ArrayList<>(path);
-                        newPath.add(neighbor);
-                        queue.add(newPath);
-                    }
-                }
-            }
-        }
-        return paths;
-    }
-
-    private Line findLineBetweenCircles(String circle1, String circle2) {
-        Map<String, Line> circleLineMap = new HashMap<>();
-        circleLineMap.put("aa_aj", aj);
-        circleLineMap.put("aa_ta", ta);
-        circleLineMap.put("hh_hj", hj);
-        circleLineMap.put("hh_ph", ph);
-        circleLineMap.put("hh_ht", ht);
-        circleLineMap.put("hh_vh", vh);
-        circleLineMap.put("jj_aj", aj);
-        circleLineMap.put("jj_hj", hj);
-        circleLineMap.put("jj_lj", lj);
-        circleLineMap.put("ll_lj", lj);
-        circleLineMap.put("ll_pl", pl);
-        circleLineMap.put("pp_ph", ph);
-        circleLineMap.put("pp_pl", pl);
-        circleLineMap.put("pp_qp", qp);
-        circleLineMap.put("qq_qp", qp);
-        circleLineMap.put("qq_sq", sq);
-        circleLineMap.put("qq_qv", qv);
-        circleLineMap.put("ss_sq", sq);
-        circleLineMap.put("ss_sz", sz);
-        circleLineMap.put("tt_ht", ht);
-        circleLineMap.put("tt_a1t", a1t);
-        circleLineMap.put("vv_vh", vh);
-        circleLineMap.put("vv_qv", qv);
-        circleLineMap.put("vv_zv", zv);
-        circleLineMap.put("vv_va1", va1);
-        circleLineMap.put("yy_zy", zy);
-        circleLineMap.put("yy_ya1", ya1);
-        circleLineMap.put("zz_sz", sz);
-        circleLineMap.put("zz_zv", zv);
-        circleLineMap.put("zz_zy", zy);
-        circleLineMap.put("a1_a1t", a1t);
-        circleLineMap.put("a1_va1", va1);
-        circleLineMap.put("a1_ya1", ya1);
-
-        Line line = circleLineMap.get(circle1 + "_" + circle2);
-        if (line == null) {
-            line = circleLineMap.get(circle2 + "_" + circle1);
-        }
-
-        return line;
-    }
-
-    private void resetLines() {
-        Line[] allLines = {qp, pl, lj, ht, ph, ta, a1t, ya1, zy, sz, va1, qv, hj, vh, zv, sq, aj};
-        for (Line line : allLines) {
-            line.setStroke(Color.BLACK);
-            line.setStrokeWidth(1);
         }
     }
 }
